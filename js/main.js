59cf3099e8b7ecbbfc9064cddb65f21a3d83f0f8
SimoneBenato <sb270298@gmail.com>
2021-07-14 16:48:25 +0200

    Primo esercizio con js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// (insicurissimo) password generator
+
+var nome = prompt("Inserisci il tuo nome");
+
+// console.log(nome)
+
+var cognome = prompt("Inserisci il tuo cognome");
+
+// console.log(cognome)
+
+var colore = prompt("Inserisci il tuo colore preferito");
+
+// console.log(colore)
+
+document.getElementById("password").innerHTML = nome + cognome + colore + "21"
